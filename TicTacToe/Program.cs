e1d1daafc9363dc1bc5c74ed4ad190034e33b5c0
you <you@you.com>
2018-07-24 20:16:57 -0400

    first commit

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        public static void draw(TTTLogic logic)
+        {
+            Console.Clear();
+            Console.WriteLine("Current Game Board -------------");
+
+            if(logic.turn() == TTTLogic.XO.O)
+            {
+                Console.WriteLine("o is turn:");
+            }
+            else if (logic.turn() == TTTLogic.XO.X)
+            {
+                Console.WriteLine("x is turn:");
+            }
+
+            for (int row=0; row<3; row++)
+            {
+                for(int col=0; col<3; col++)
+                {
+                   TTTLogic.XO xo =logic.Getpiece(col, row);
+                    if(xo == TTTLogic.XO.Empty)
+                    {
+                        Console.Write(" ");
+                    }
+                    if(xo == TTTLogic.XO.O)
+                    {
+                        Console.Write("o");
+                    }
+                    if(xo == TTTLogic.XO.X)
+                    {
+                        Console.Write("x");
+                    }
+                    
+                }
+                Console.WriteLine("|");
+            }
+        }
+        static void OnError()
+        {
+            throw new Exception("Something done messed up");
+        }
+        static void Main(string[] args)
+        {
+            TTTLogic ttt = new TTTLogic(Program.OnError);
+
+            int x;
+            int y;
+
+            while(true)
+            {
+                draw(ttt);
+                do
+                {
+                    Console.WriteLine("Enter your X position: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                } while (!TTTLogic.IsValidCol(x));
+
+                do
+                {
+                    Console.WriteLine("Enter your Y position: ");
+                    y = Convert.ToInt32(Console.ReadLine());
+                } while (!TTTLogic.IsValidRow(y));
+
+                // it is guaranteed that x and y are valid rows/columns, or else the loops wouldn't have exited
+
+                ttt.placingpiece(x, y);
+            }
+            
+
+                
+        }
+    }
+}
---
+++ TicTacToe/TTTLogic.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class TTTLogic
+    {
+        public enum XO
+        {
+            X,
+            O,
+            Empty,
+        }
+        XO NextMove = XO.X;
+
+        public XO turn()
+        {
+            return NextMove;
+        }
+
+        XO[,] board = new XO[3, 3] {
+            {XO.Empty, XO.Empty, XO.Empty },
+            {XO.Empty, XO.Empty, XO.Empty },
+            {XO.Empty, XO.Empty, XO.Empty },
+        };
+
+        public delegate void OnIllegalMove();
+
+        OnIllegalMove myIllegalMoveHandler;
+        public TTTLogic(OnIllegalMove fnOnIllegalMove)
+        {
+            myIllegalMoveHandler = fnOnIllegalMove;
+        }
+
+        public static bool IsValidRow(int y)
+        {
+            return y >= 0 && y <= 2;
+        }
+        public static bool IsValidCol(int x)
+        {
+            return x >= 0 && x <= 2;
+        }
+
+        public void placingpiece(int positionX, int positionY)
+        {
+            XO thispiece = turn();
+            if(positionX <0)
+            {
+                myIllegalMoveHandler();
+                return;
+            }
+            else if(positionY<0)
+            {
+                myIllegalMoveHandler();
+                return;
+            }
+            else if(positionX > 2)
+            {
+                myIllegalMoveHandler();
+                return;
+
+            }
+            else if(positionY > 2)
+            {
+                myIllegalMoveHandler();
+                return;
+            }
+            else if(board[positionX, positionY] != XO.Empty)
+            {
+                myIllegalMoveHandler();
+                return;
+            }
+            
+            if(this.NextMove == XO.X)
+            {
+                this.NextMove = XO.O;
+            }
+            else if (this.NextMove == XO.O)
+            {
+                this.NextMove = XO.X;
+            }
+            board[positionX, positionY] = thispiece;
+        }
+
+        public XO Getpiece(int positionX, int positionY)
+        {
+            return board[positionX, positionY];
+        }
+    }
+}
